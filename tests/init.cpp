fa87507d0c01d95bee556f13a7259da1f15cf5b5
Yana Grishkova <yanaxgrishkova@gmail.com>
2017-06-15 06:57:46 +0300

    Create init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -13,3 +13,18 @@ SCENARIO("insert")
   rbt.insert(5);
   REQUIRE(rbt.findElement(5) != 0);
 }
+
+SCENARIO("insert2", "[init]")
+{
+  RedBlackTree<int> rbt;
+  rbt.insert(2);
+  rbt.insert(3);
+  rbt.insert(4);
+  REQUIRE(tree._root() == tree.findElement(3));
+  REQUIRE(tree._color(3) == 1);
+  REQUIRE(tree._color(2) == 0);
+  REQUIRE(tree._color(4) == 0);
+  REQUIRE(tree.findElement(2) != 0);
+  REQUIRE(tree.findElement(3) != 0);
+  REQUIRE(tree.findElement(4) != 0);
+}
